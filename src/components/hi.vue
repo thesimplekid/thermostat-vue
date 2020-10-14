3b49b20246ba798f6da03e635d45cbf3e67e4c2a
thesimplekid <thesimplekid@brendanmurphy.xyz>
2020-10-13 20:59:17 -0400

    first test

---
+++ src/components/hi.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ idk }}</h1>
+  <h2>hello worlf</h2>
+  <h3>{{ tester }}</h3>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    idk: String,
+    tester: Number
+  }
+})
+export default class Hi extends Vue {
+  idk!: string;
+  tester = 12;
+}
+</script>
+
+<style scoped></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Hi idk="test" />
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Hi from "@/components/hi.vue";
+
+@Options({
+  components: {
+    Hi
+  }
+})
+export default class About extends Vue {}
+</script>

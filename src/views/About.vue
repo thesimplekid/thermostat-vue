31b96fb268e15924aded2c9d78aa455bb7cf8430
thesimplekid <thesimplekid@brendanmurphy.xyz>
2020-10-17 19:16:16 -0400

    delete hi

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <Hi idk="dxfcgvhbjnkm" />
-    <Zonestatus />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import Hi from "@/components/hi.vue";
-import Zonestatus from "@/components/zonestatus.vue";
 
 @Options({
-  components: {
-    Hi,
-    Zonestatus
-  }
+  components: {}
 })
 export default class About extends Vue {}
 </script>
